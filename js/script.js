a39e647c6a9f8c819f167d4714ebe3539ef49bfe
Brayan Martinez <brayan27_4@live.com>
2023-04-17 19:23:43 -0500

    Changes pre-edit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Desplegar navbar de menu hamburguesa
 const menuIcon = document.querySelector('.menu-icon');
 const menu = document.querySelector('nav ul');
 
@@ -5,3 +6,53 @@ menuIcon.addEventListener('click', () => {
 	menu.classList.toggle('show');
 });
 
+// Código a implementar Cards
+const cards = document.querySelectorAll('.card');
+const pagination = document.getElementById('pagination');
+
+let currentPage = 1;
+let cardsPerPage = 6;
+
+function showCards(cards, page, cardsPerPage) {
+   for (let i = 0; i < cards.length; i++) {
+      cards[i].classList.add('hide');
+      if (i >= (page * cardsPerPage) - cardsPerPage && i < page * cardsPerPage) {
+         cards[i].classList.remove('hide');
+      }
+   }
+}
+
+function createPagination(cards, cardsPerPage) {
+   let pages = Math.ceil(cards.length / cardsPerPage);
+   for (let i = 1; i <= pages; i++) {
+      let li = document.createElement('li');
+      let a = document.createElement('a');
+      a.href = '#';
+      a.innerHTML = i;
+      li.appendChild(a);
+      pagination.appendChild(li);
+
+      a.addEventListener('click', function() {
+         currentPage = i;
+         showCards(cards, currentPage, cardsPerPage);
+      });
+   }
+
+   let li = document.createElement('li');
+   let a = document.createElement('a');
+   a.href = '#';
+   a.innerHTML = '>';
+   li.appendChild(a);
+   pagination.appendChild(li);
+
+   a.addEventListener('click', function() {
+      if (currentPage < pages) {
+         currentPage++;
+         showCards(cards, currentPage, cardsPerPage);
+      }
+   });
+
+   showCards(cards, currentPage, cardsPerPage);
+}
+
+createPagination(cards, cardsPerPage);
